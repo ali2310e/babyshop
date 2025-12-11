c73eb8e3dd45ea8513808e09637e7afed2a2599a
ali2310e <ali2310e@aptechsite.net>
2025-12-11 18:12:34 +0500

    .

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -39,7 +39,7 @@ class _SigninScreenState extends State<SigninScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.transparent,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
